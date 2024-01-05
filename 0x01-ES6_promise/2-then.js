3946469d30e5762600fec3f0954ec021ef1ad387
TeeMeeLehin <samemerald8@gmail.com>
2024-01-05 14:17:14 +0100

    added task

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -11,7 +11,6 @@ export default function handleResponseFromAPI(promise) {
         return successObject;
       })
       .catch((error) => {
-        console.error('Error:', error.message);
         reject(new Error()); // Return an empty Error object for rejection
       });
   });
