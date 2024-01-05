f66949e7d53e225b4e02fd73f3fb184cecd22c7f
TeeMeeLehin <samemerald8@gmail.com>
2024-01-05 14:24:50 +0100

    added task

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,17 +1,12 @@
 export default function handleResponseFromAPI(promise) {
-  return new Promise((resolve, reject) => {
-    promise
-      .then(() => {
-        console.log('Got a response from the API');
-        const successObject = {
-          status: 200,
-          body: 'success',
-        };
-        resolve(successObject);
-        return successObject;
-      })
-      .catch((error) => {
-        reject(new Error()); // Return an empty Error object for rejection
-      });
-  });
+  return promise
+    .then(() => {
+      const successObject = {
+        status: 200,
+        body: 'success',
+      };
+      return successObject;
+    })
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
 }
