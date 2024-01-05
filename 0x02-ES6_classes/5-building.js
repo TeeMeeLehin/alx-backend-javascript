d65860077cb6a3eda8d8e2a5f1d08f27ac8838a8
TeeMeeLehin <samemerald8@gmail.com>
2024-01-05 10:15:39 +0100

    added task

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -2,11 +2,19 @@
 ["evacuationWarningMessage"] }] */
 export default class Building {
   constructor(sqft) {
+    if (new.target !== Building
+      && this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
     this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
+  }
+
+  set sqft(newSqft) {
+    this._sqft = newSqft;
   }
 
   evacuationWarningMessage() {
