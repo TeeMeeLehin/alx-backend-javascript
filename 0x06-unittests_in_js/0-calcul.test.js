656b462b6f25179aad9227e6f8261affb11e3db0
TeeMeeLehin <samemerald8@gmail.com>
2024-02-29 13:11:38 +0100

    added files

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,7 +1,27 @@
 const calculateNumber = require("./0-calcul.js");
 const assert = require("assert");
 
-describe("calculateNumber test", function() {
+describe("calculateNumber", function() {
+  it("summing integers", function() {
+    assert.equal(calculateNumber(6, 7), 13);
+    assert.equal(calculateNumber(4, 5), 9);
+    assert.equal(calculateNumber(5, 5), 10);
+  });
+  it("rounding up correctly", function() {
+    assert.equal(calculateNumber(6.9, 7), 14);
+    assert.equal(calculateNumber(3.5, 5.0), 9);
+    assert.equal(calculateNumber(4.2, 5.7), 10);
+  });
+  it("rounding down correctly", function() {
+    assert.equal(calculateNumber(6.0, 7.0), 13);
+    assert.equal(calculateNumber(4.2, 5.4), 9);
+    assert.equal(calculateNumber(4.2, 7), 11);
+  });
+  it("summing negative integers", function() {
+    assert.equal(calculateNumber(-1, 7), 6);
+    assert.equal(calculateNumber(4, -15), -11);
+    assert.equal(calculateNumber(-9, -8), -17);
+  });
   it("checks equality", function() {
     assert.equal(calculateNumber(6, 7), 13);
     assert.equal(calculateNumber(4.2, 5), 9);
