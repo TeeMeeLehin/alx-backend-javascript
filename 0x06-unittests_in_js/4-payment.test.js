484cf89d7217f716bc03392e10a86d57c9e76116
TeeMeeLehin <samemerald8@gmail.com>
2024-02-29 14:26:40 +0100

    added files

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,20 @@
+const sinon = require('sinon');
+const assert = require("assert");
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
+
+describe('sendPaymentRequestToApi', () => {
+  it('sendPaymentRequestToApi uses the calculateNumber method of Utils', () => {
+    const spyVar = sinon.spy(console);
+
+    const CNStub = sinon.stub(Utils, "calculateNumber").returns(10);
+
+    sendPaymentRequestToApi(100, 20);
+    assert(CNStub.calledWith('SUM', 100, 20));
+    assert.strictEqual(CNStub.callCount, 1);
+    assert(spyVar.log.calledWith('The total is: 10'));
+    assert.strict(spyVar.log.callCount, 1)
+    spyVar.log.restore();
+    CNStub.restore();
+  });
+});
