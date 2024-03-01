01c644a9935d5d906b3aa18776f9c56b761a782e
TeeMeeLehin <samemerald8@gmail.com>
2024-03-01 20:54:05 +0100

    added task

---
+++ 0x06-unittests_in_js/10-api/api.js
@@ -0,0 +1,37 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the payment system');
+  });
+
+app.get('/cart/:id([0-9]+)', (req, res) => {
+    res.send(`Payment methods for cart ${req.params.id}`);
+  });
+
+app.get('/available_payments', (req, res) => {
+  res.set("Content-Type", "application/json");
+  const paymentOptions = {
+  payment_methods: {
+        credit_cards: true,
+        paypal: false
+  }
+  }
+  res.send(paymentOptions);
+});
+
+app.post('/login', (req, res) => {
+  const user = req.body.user || req.body.userName;
+  if (user) {
+    res.send(`Welcome ${user}`);
+  } else {
+    res.status(404).send();
+  }
+});
+
+app.listen(7865, () => {
+    console.log('API available on localhost port 7865');
+});
+
+module.exports = app;
