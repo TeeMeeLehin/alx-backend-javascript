4bf2e020fd72719ae53f5b3276afbb8db651e1af
TeeMeeLehin <samemerald8@gmail.com>
2024-03-01 20:23:39 +0100

    added task

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -1,20 +1,29 @@
 const sinon = require('sinon');
 const assert = require("assert");
 const Utils = require('./utils');
-const sendPaymentRequestToApi = require('./4-payment');
+const sendPaymentRequestToApi = require('./5-payment');
 
 describe('sendPaymentRequestToApi', () => {
+  let spyVar;
+
+  beforeEach(function () {
+    spyVar = sinon.spy(console, 'log');
+  })
+
+  afterEach(function () {
+    spyVar.restore();
+  })
+
   it('sendPaymentRequestToApi uses the calculateNumber method of Utils', () => {
-    const spyVar = sinon.spy(console);
-
-    const CNStub = sinon.stub(Utils, "calculateNumber").returns(10);
-
     sendPaymentRequestToApi(100, 20);
-    assert(CNStub.calledWith('SUM', 100, 20));
-    assert.strictEqual(CNStub.callCount, 1);
-    assert(spyVar.log.calledWith('The total is: 10'));
-    assert.strict(spyVar.log.callCount, 1)
-    spyVar.log.restore();
-    CNStub.restore();
+    assert(spyVar.calledWith('The total is: 120'));
+    assert.strict(spyVar.callCount, 1)
   });
+
+  it('sendPaymentRequestToApi uses the calculateNumber method of Utils', () => {
+    sendPaymentRequestToApi(10, 10);
+    assert(spyVar.calledWith('The total is: 20'));
+    assert.strict(spyVar.callCount, 1)
+  });
+
 });
